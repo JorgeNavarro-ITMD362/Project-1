76762e4b75ebac631a95dbf0bc0a9b6fcf9cd42c
jorgenavarro1 <jnavarro11@hawk.iit.edu>
2024-10-11 18:46:44 -0500

    changing color on js when hover over button

--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
 document.body.addEventListener('mouseout', e => {
   let button = e.target.closest('button');
   if (!button) { return; }
-  button.style.backgroundColor = 'rgb(91, 100, 10)';
+  button.style.backgroundColor = 'rgb(136, 40, 57)';
 });
